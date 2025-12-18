357a39e747b1124a90a0a0c42d65d4641fbe2d29
naumannaikwade <naumannaikwade25@gmail.com>
2025-12-18 11:04:38 +0530

    feat: updated env variables for production

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ connectDB();
 
 // CORS configuration for production
 const corsOptions = {
-  origin: 'https://minieventplatformbynauman.vercel.app/',
+  origin: 'https://minieventplatformbynauman.vercel.app',
   credentials: true,
   optionsSuccessStatus: 200
 };
